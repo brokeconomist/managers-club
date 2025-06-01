facf37ecf3b45bd57e62aa2fbcd2483831d7ee34
brokeconomist <brokeconomist@gmail.com>
2025-06-01 16:10:07 +0300

    Update discount_cash_tool.py

--- discount_cash_tool.py
+++ discount_cash_tool.py
@@ -10,7 +10,9 @@ def calculate_discount_cash_tool(
     days_pay_no_discount,
     days_pay_cash,
     cost_pct,
-    capital_cost
+    capital_cost,
+    avg_days_suppliers_payment,
+    avg_days_receivable_current
 ):
     # Μετατροπή ποσοστών σε δεκαδικά
     discount = discount_pct / 100
@@ -19,40 +21,57 @@ def calculate_discount_cash_tool(
     cost = cost_pct / 100
     capital_cost_rate = capital_cost / 100
 
+    new_total_sales = current_sales + additional_sales
+
+    # Προεξοφλητικοί παράγοντες για πληρωμή τοις μετρητοίς και χωρίς έκπτωση
+    discount_factor_cash = 1 / (1 + (capital_cost_rate / 365)) ** days_pay_cash
+    discount_factor_no_discount = 1 / (1 + (capital_cost_rate / 365)) ** days_pay_no_discount
+
+    # Υπολογισμός παρούσας αξίας εισπράξεων με νέα πολιτική πληρωμών
+    pv_receivables = (
+        new_total_sales * pct_accept * (1 - discount) * discount_factor_cash
+        + new_total_sales * pct_reject * discount_factor_no_discount
+    )
+
+    # Κόστος πωλήσεων αναλογικά με τις επιπλέον πωλήσεις
+    cost_of_sales = cost * (additional_sales / current_sales) * current_sales
+
+    # Προεξόφληση κόστους πωλήσεων με βάση μέση περίοδο αποπληρωμής προμηθευτών
+    discount_factor_suppliers = 1 / (1 + (capital_cost_rate / 365)) ** avg_days_suppliers_payment
+    pv_cost_of_sales = cost_of_sales * discount_factor_suppliers
+
+    # Προεξόφληση τρεχουσών πωλήσεων με βάση τρέχουσα μέση περίοδο είσπραξης
+    discount_factor_receivable_current = 1 / (1 + (capital_cost_rate / 365)) ** avg_days_receivable_current
+    pv_current_sales = current_sales * discount_factor_receivable_current
+
+    # Τελικός υπολογισμός ΚΠΑ
+    npv = pv_receivables - pv_cost_of_sales - pv_current_sales
+
     # Κέρδος από επιπλέον πωλήσεις χωρίς έκπτωση (κόστος από επιπλέον πωλήσεις)
     profit_additional_sales = additional_sales * (1 - cost)
 
-    # Νέο σύνολο πωλήσεων μετά την αύξηση
-    new_total_sales = current_sales + additional_sales
-
-    # Μέση περίοδος είσπραξης πριν
-    avg_days_before = pct_accept * days_pay_discount + pct_reject * days_pay_no_discount
-
-    # Μέση περίοδος είσπραξης μετά την εφαρμογή της έκπτωσης (λαμβάνοντας υπόψη και το % πελατών που θα ακολουθήσουν τη νέα πολιτική)
-    avg_days_after = (pct_accept * days_pay_discount + pct_reject * days_pay_no_discount)  # χωρίς % πελατών που ακολουθούν νέα πολιτική
-
-    # *** ΑΦΑΙΡΕΜΕΝΟ το κομμάτι αποδέσμευσης κεφαλαίων ***
-
-    # Υπολογισμός NPV (προεξόφληση κεφαλαίου)
-    # Θα υπολογίσουμε NPV μόνο ως κέρδος προεξόφλησης έκπτωσης επί επιπλέον πωλήσεις
-
-    # Ας υποθέσουμε NPV σαν κέρδος από μείωση εκπτώσεων (απλοποιημένο παράδειγμα):
-    npv = profit_additional_sales * capital_cost_rate  # απλοποιημένος υπολογισμός
-
-    # Μέγιστη έκπτωση που ισοσκελίζει το NPV (Break-Even discount)
+    # Μέγιστη έκπτωση break-even (όπως πριν)
     if additional_sales != 0:
         max_discount_break_even = profit_additional_sales / additional_sales
     else:
         max_discount_break_even = 0
 
-    # Βέλτιστη έκπτωση: 25% του μέγιστου
+    # Βέλτιστη έκπτωση 25% του μέγιστου
     optimal_discount = max_discount_break_even * 0.25
 
+    # Μετατροπή σε ποσοστά
     max_discount_break_even_pct = max_discount_break_even * 100
     optimal_discount_pct = optimal_discount * 100
 
+    # Υπολογισμός αποδέσμευσης κεφαλαίου (όπως πριν)
+    avg_days_before = pct_accept * days_pay_discount + pct_reject * days_pay_no_discount
+    capital_before = current_sales * cost * avg_days_before / 365
+    capital_after = new_total_sales * cost * avg_days_before / 365
+    released_capital = capital_before - capital_after
+
     return {
         "profit_additional_sales": profit_additional_sales,
+        "released_capital": released_capital,
         "npv": npv,
         "max_discount_break_even_pct": max_discount_break_even_pct,
         "optimal_discount_pct": optimal_discount_pct,
