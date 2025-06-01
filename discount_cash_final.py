e93a80f60750601368dfd244a95e45d7f8df6fe7
brokeconomist <brokeconomist@gmail.com>
2025-06-02 01:52:27 +0300

    Update discount_cash_final.py

--- discount_cash_final.py
+++ discount_cash_final.py
@@ -12,19 +12,15 @@ def calculate_discount_cash_fixed_pct(
     avg_supplier_pay_days,
     current_collection_days
 ):
-    # Σύνολο νέων πωλήσεων
     total_sales = current_sales + extra_sales
 
-    # Προεξοφλητικός συντελεστής
     def discount_factor(days):
         return 1 / pow(1 + cost_of_capital_annual / 365, days)
 
-    # Σταθμισμένο ποσοστό αποδοχής στο ΣΥΝΟΛΟ νέων πωλήσεων
     weighted_pct_discounted_total = (
         (current_sales * pct_customers_accept) + extra_sales
     ) / (current_sales + extra_sales)
 
-    # Παρούσα αξία πωλήσεων σε πελάτες που αποδέχονται την έκπτωση
     pv_discount_customers = (
         total_sales
         * weighted_pct_discounted_total
@@ -32,14 +28,12 @@ def calculate_discount_cash_fixed_pct(
         * discount_factor(days_cash)
     )
 
-    # Παρούσα αξία πωλήσεων σε πελάτες που ΔΕΝ αποδέχονται την έκπτωση
     pv_other_customers = (
         total_sales
         * (1 - weighted_pct_discounted_total)
         * discount_factor(days_reject)
     )
 
-    # Παρούσα αξία κόστους επιπλέον πωλήσεων
     pv_cost_extra_sales = (
         cost_of_sales_pct
         * (extra_sales / current_sales)
@@ -47,41 +41,36 @@ def calculate_discount_cash_fixed_pct(
         * discount_factor(avg_supplier_pay_days)
     )
 
-    # Παρούσα αξία τρεχουσών πωλήσεων
     pv_current_sales = current_sales * discount_factor(current_collection_days)
 
-    # ✅ Τελικό NPV
     npv = pv_discount_customers + pv_other_customers - pv_cost_extra_sales - pv_current_sales
 
-    # ✅ Υπολογισμός μέσης νέας περιόδου είσπραξης
-    new_weighted_collection_days = (
-        days_cash * pct_customers_accept +
-        days_reject * (1 - pct_customers_accept)
-    )
-
-    # ✅ Υπολογισμός μέγιστης δυνητικής έκπτωσης (NPV = 0)
+    # Μέγιστη Έκπτωση - ΝΕΟΣ τύπος από Excel
     r = cost_of_capital_annual
-    D = new_weighted_collection_days
-    d = days_cash
-    cogs_pct = cost_of_sales_pct
-    extra_ratio = extra_sales / current_sales
+    P3 = current_sales
+    P4 = extra_sales
+    P5 = cash_discount_rate
+    P9 = days_reject
+    P10 = days_cash
+    P11 = cost_of_sales_pct
+    P12 = r
+    P13 = avg_supplier_pay_days
+    P15 = current_collection_days
+    P20 = pct_customers_accept
 
-    # Τύπος Excel από το βιβλίο
-    numerator = 1 - 1 / (1 + extra_ratio)
-    denominator = pow(1 + r / 365, D - d) * (
-        numerator +
-        (
-            pow(1 + r / 365, d - current_collection_days)
-            + cogs_pct * extra_ratio * pow(1 + r / 365, d - avg_supplier_pay_days)
-        ) / (1 + extra_ratio)
+    denom_inner = (
+        (1 - (1 / P20))
+        + (
+            pow(1 + P12 / 365, P9 - P15)
+            + P11 * (P4 / P3) * pow(1 + P12 / 365, P9 - P13)
+        ) / (P20 * (1 + (P4 / P3)))
     )
-    max_discount = 1 - denominator
 
-    # ✅ Βέλτιστη έκπτωση
-    optimal_discount = (1 - pow(1 + r / 365, d - current_collection_days)) / 2
+    max_discount = 1 - pow(1 + P12 / 365, P10 - P9) * denom_inner
+    optimal_discount = (1 - pow(1 + P12 / 365, P10 - P15)) / 2
 
     return {
         "NPV": round(npv, 2),
         "Max Discount %": round(max_discount * 100, 2),
-        "Optimal Discount %": round(optimal_discount * 100, 2),
+        "Optimal Discount %": round(optimal_discount * 100, 2)
     }
