805de15ea239574b897c5bd8dde9517afaa5130a
brokeconomist <brokeconomist@gmail.com>
2025-06-05 09:35:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from clv_calculator import show_clv_calculator
 from substitution_analysis import show_substitution_analysis
 from complementary_analysis import show_complementary_analysis
 from loss_threshold import show_loss_threshold_before_price_cut
+from discount_cash_analysis import show_discount_cash_analysis  # ΝΕΟ
 
 
 st.set_page_config(page_title="Managers’ Club", page_icon="📊", layout="centered")
@@ -37,3 +38,5 @@ elif tool == "➕ Ανάλυση Συμπληρωματικών Προϊόντω
     show_complementary_analysis()
 elif tool == "📉 Όριο Απώλειας Πωλήσεων πριν τη Μείωση Τιμών":
     show_loss_threshold_before_price_cut()
+elif tool == "Αποδοτικότητα Έκπτωσης Τοις Μετρητοίς":
+    show_discount_cash_analysis()  # ΝΕΟ
