b10ac7179ba2d401993922f5639ce8ea1414716b
brokeconomist <brokeconomist@gmail.com>
2025-06-05 21:24:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ tools = {
     "📉 Όριο Απώλειας Πωλήσεων πριν τη Μείωση Τιμών": show_loss_threshold_before_price_cut,
     "🕒 Ανάλυση Αύξησης Πίστωσης": show_credit_extension_analysis,
     "🏛️ Ανάλυση Πολιτικής Πίστωσης": show_credit_policy_analysis,
-    "🏦 Ανάλυση Πίστωσης Προμηθευτών (Έκπτωση)": show_supplier_credit_analysis  # ✅ Νέο
+    "🏦 Ανάλυση Πίστωσης Προμηθευτών (Έκπτωση)": show_supplier_credit_analysis,  # ✅ Νέο
     "📊 Ταμειακός Κύκλος": run_cash_cycle_app  # ✅ Νέο
 }
 
