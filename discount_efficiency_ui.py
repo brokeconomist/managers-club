7ac51e0c177b4765db27e9c0c339fbf717fad33e
brokeconomist <brokeconomist@gmail.com>
2025-06-14 17:56:01 +0300

    Update discount_efficiency_ui.py

--- discount_efficiency_ui.py
+++ discount_efficiency_ui.py
@@ -24,7 +24,7 @@ def show_discount_efficiency_ui():
 
     st.markdown("---")
 
-    # Υπολογισμοί
+    # Μέση περίοδος είσπραξης πριν την αλλαγή
     current_avg_collection = days_accept_discount * pct_accept_discount + days_reject_discount * pct_reject_discount
     current_receivables = current_sales * current_avg_collection / 365
 
@@ -33,20 +33,29 @@ def show_discount_efficiency_ui():
     new_receivables_discount = current_sales * new_avg_collection_discount / 365
     released_capital_discount = current_receivables - new_receivables_discount
 
+    # Ποσοστό πελατών που ακολουθούν τη νέα πολιτική (μόνο επί των νέων πωλήσεων)
     pct_follow_new_policy = ((current_sales * pct_accept_discount) + extra_sales) / (current_sales + extra_sales)
     pct_remain_old = 1 - pct_follow_new_policy
 
+    # Νέα μέση περίοδος είσπραξης μετά αύξηση πωλήσεων
     new_avg_collection_after_increase = pct_follow_new_policy * days_cash_payment + pct_remain_old * days_reject_discount
     receivables_after_increase = ((current_sales + extra_sales) * new_avg_collection_after_increase) / 365
     released_capital_after_increase = current_receivables - receivables_after_increase
 
+    # Κέρδος από επιπλέον πωλήσεις (χωρίς έκπτωση)
     profit_extra_sales = extra_sales * (1 - cost_of_sales_pct)
+
+    # Κέρδος από αποδεσμευμένο κεφάλαιο
     profit_released_capital = released_capital_after_increase * wacc
-    discount_cost = (current_sales + extra_sales) * pct_follow_new_policy * cash_discount_pct
+
+    # Κόστος έκπτωσης ΜΟΝΟ επί των νέων πωλήσεων
+    discount_cost = extra_sales * pct_follow_new_policy * cash_discount_pct
+
     total_profit = profit_extra_sales + profit_released_capital - discount_cost
 
     discount_rate_daily = wacc / 365
 
+    # Υπολογισμός NPV
     npv = (
         (current_sales + extra_sales) * pct_follow_new_policy * (1 - cash_discount_pct)
         * (1 / (1 + discount_rate_daily) ** days_cash_payment)
@@ -57,6 +66,7 @@ def show_discount_efficiency_ui():
     )
 
     try:
+        # Μέγιστη έκπτωση (NPV Break Even) ΜΟΝΟ επί των νέων πωλήσεων
         max_discount_break_even = 1 - (1 + discount_rate_daily) ** (days_cash_payment - days_reject_discount) * (
             ((1 - (1 / pct_follow_new_policy)) +
              ((1 + discount_rate_daily) ** (days_reject_discount - current_avg_collection) + (extra_sales / current_sales) * (1 + discount_rate_daily) ** (days_reject_discount - supplier_payment_days))) /
@@ -72,7 +82,6 @@ def show_discount_efficiency_ui():
     st.write(f"**Μέση περίοδος είσπραξης πριν τη νέα πολιτική:** {format_number_gr(current_avg_collection)} μέρες")
     st.write(f"**Τρέχουσες απαιτήσεις πριν τη νέα πολιτική:** {format_number_gr(current_receivables)} €")
 
-
     st.write(f"**% πελατών που ακολουθεί τη νέα πολιτική επί του νέου συνόλου:** {format_percentage_gr(pct_follow_new_policy)}")
     st.write(f"**% πελατών που παραμένει με την παλιά κατάσταση:** {format_percentage_gr(pct_remain_old)}")
 
@@ -80,12 +89,12 @@ def show_discount_efficiency_ui():
     st.write(f"**Απαιτήσεις μετά την αύξηση πωλήσεων:** {format_number_gr(receivables_after_increase)} €")
 
     st.write(f"**Κέρδος από επιπλέον πωλήσεις:** {format_number_gr(profit_extra_sales)} €")
-    st.write(f"**Κόστος έκπτωσης:** {format_number_gr(discount_cost)} €")
+    st.write(f"**Κόστος έκπτωσης (μόνο επί των νέων πωλήσεων):** {format_number_gr(discount_cost)} €")
 
     st.write(f"**NPV:** {format_number_gr(npv)} €")
 
     if max_discount_break_even is not None:
-        st.write(f"**Μέγιστη έκπτωση (NPV Break Even):** {format_percentage_gr(max_discount_break_even)}")
+        st.write(f"**Μέγιστη έκπτωση (NPV Break Even) επί των νέων πωλήσεων:** {format_percentage_gr(max_discount_break_even)}")
     else:
         st.write("**Μέγιστη έκπτωση (NPV Break Even):** Δεν υπολογίζεται (διαίρεση με μηδέν)")
 
