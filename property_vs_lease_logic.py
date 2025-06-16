bbde8158da0956cf543095094f48b5f233fefcfe
brokeconomist <brokeconomist@gmail.com>
2025-06-16 13:42:52 +0300

    Update property_vs_lease_logic.py

--- property_vs_lease_logic.py
+++ property_vs_lease_logic.py
@@ -1,5 +1,30 @@
 import numpy as np
-import numpy_financial as npf
+
+def npv(rate, cashflows):
+    """
+    Υπολογίζει την Καθαρή Παρούσα Αξία (NPV).
+    rate: επιτόκιο ανά περίοδο (π.χ. μηνιαίο, σε δεκαδική μορφή)
+    cashflows: λίστα ή numpy array με χρηματικές ροές (αρνητικές/θετικές)
+    """
+    periods = np.arange(len(cashflows))
+    discounted = cashflows / ((1 + rate) ** periods)
+    return discounted.sum()
+
+def pmt(rate, nper, pv, fv=0, when='end'):
+    """
+    Υπολογίζει τη σταθερή πληρωμή δανείου (PMT).
+    rate: επιτόκιο ανά περίοδο (π.χ. μηνιαίο, σε δεκαδική μορφή)
+    nper: αριθμός περιόδων
+    pv: παρούσα αξία (ποσό δανείου, θετικό)
+    fv: μελλοντική αξία (συνήθως 0)
+    when: 'end' για πληρωμή στο τέλος περιόδου, 'begin' για αρχή
+    """
+    if rate == 0:
+        return -(pv + fv) / nper
+    else:
+        when_val = 1 if when == 'begin' else 0
+        payment = -(rate * (pv * (1 + rate)**nper + fv)) / ((1 + rate * when_val) * ((1 + rate)**nper - 1))
+        return payment
 
 def calculate_property_vs_lease(
     rent_per_month, years, annual_rate, property_price,
@@ -12,22 +37,22 @@ def calculate_property_vs_lease(
     # Μίσθωση
     total_rent = rent_per_month * 12 * years
     rent_cashflows = np.full(months, -rent_per_month)
-    rent_npv = npf.npv(monthly_rate, rent_cashflows)
+    rent_npv = npv(monthly_rate, rent_cashflows)
 
     # Δάνειο για αγορά
     loan_amount = property_price
-    monthly_payment = npf.pmt(monthly_rate, months, -loan_amount)
+    monthly_payment = pmt(monthly_rate, months, -loan_amount)
     total_loan_payments = monthly_payment * months
     total_interest = total_loan_payments - loan_amount
 
     # Φορολογικά εκπιπτόμενα
     annual_depreciation = (property_price + acquisition_costs) / years
     annual_tax_shield = (
-        (total_interest / years) + annual_depreciation + annual_maintenance
+        total_interest / years + annual_depreciation + annual_maintenance
     ) * tax_rate_decimal
     total_tax_savings = annual_tax_shield * years
 
-    # Συνολικό κόστος απόκτησης (με φόρους)
+    # Συνολικό κόστος απόκτησης
     net_acquisition_cost = total_loan_payments + acquisition_costs - total_tax_savings
 
     return {
