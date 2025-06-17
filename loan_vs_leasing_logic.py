52b770fd0193ea4f907725b015321d409e6aed41
brokeconomist <brokeconomist@gmail.com>
2025-06-17 05:55:09 +0300

    Update and rename property_vs_lease_logic.py to loan_vs_leasing_logic.py

---
+++ loan_vs_leasing_logic.py
@@ -0,0 +1,19 @@
+from utils import format_number_gr
+
+def pv(rate, nper, pmt, fv=0, when=1):
+    """Υπολογισμός παρούσας αξίας ταμειακών ροών"""
+    if rate == 0:
+        return -pmt * nper - fv
+    return -pmt * ((1 - (1 + rate) ** -nper) / rate) - fv / ((1 + rate) ** nper) if when == 0 else \
+           -pmt * (((1 - (1 + rate) ** -nper) / rate) * (1 + rate)) - fv / ((1 + rate) ** nper)
+
+def limited_depreciation(asset_value, additional_costs, dep_years, finance_years):
+    total_cost = asset_value + additional_costs
+    return min(dep_years, finance_years) * (total_cost / dep_years)
+
+def tax_savings(rate, years, interest, depreciation, tax_rate):
+    annual_deductible = (interest + depreciation) / years
+    return pv(rate, years, -annual_deductible, 0, 0) * tax_rate
+
+def total_cost(pv_installments, pv_working_cap, extra_costs, tax_benefit):
+    return pv_installments + pv_working_cap + extra_costs - tax_benefit
